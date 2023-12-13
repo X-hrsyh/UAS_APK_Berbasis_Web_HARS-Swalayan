f86194141f65fcdda837704ce6cc35738a636209
X-hrsyh <mgame4715@gmail.com>
2023-12-13 15:59:10 +0700

    first commit

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Repost by © 2023 by <a href="https://github.com/X-hrsyh" target="_blank">X-hrsyh</a></span>
+		</div>
+	</div>
+</footer>
